d0419331c55ddd37495ded6c23838a8de84b5e2d
Jorge Ramos <jorginhodeveloper@gmail.com>
2019-04-25 12:30:09 -0300

    Add files to lesson 04 - Classes

--- main.js
+++ main.js
@@ -1,9 +1,45 @@
-class Teste {
-  metodo() {
-
+/*
+  EXEMPLO SIMPLES DE USO DE CLASSES
+*/
+class List {
+  constructor() {
+    this.data = [];
   }
 
-  outro() {
-
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
   }
 }
+
+class TodoList extends List {
+  constructor() {
+    super();
+
+    this.usuario = 'Jorge';
+  }
+
+  mostraUsuario() {
+    console.log(this.usuario);
+  }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novoTodo').onclick = function() {
+  MinhaLista.add('Novo Todo');
+}
+
+MinhaLista.mostraUsuario();
+
+/*
+  EXEMPLO MÉTODO ESTÁTICO
+*/
+
+class Matematica {
+  static soma(a, b) {
+    return a + b;
+  }
+}
+
+console.log(Matematica.soma(1, 2));
