bdbc3897fcad5562a3162852a52a5ccf94da5b89
Jorge Ramos <jorginhodeveloper@gmail.com>
2019-04-25 16:09:41 -0300

    Add files to lesson 08 - valores padrão

--- bundle.js
+++ bundle.js
@@ -1,16 +1,10 @@
 "use strict";
 
-// map
-var arr = [1, 3, 4, 5, 6];
-var newArr = arr.map(function (item) {
-  return item * 2;
-});
-console.log(newArr);
-
-var teste = function teste() {
-  return {
-    nome: 'Jorge'
-  };
+var soma = function soma() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
 };
 
-console.log(teste());
+console.log(soma(1));
+console.log(soma());
--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-// map
-const arr = [1, 3, 4, 5, 6];
+const soma = (a = 3, b = 6) => a + b;
 
-const newArr = arr.map(item => item * 2);
-console.log(newArr);
-
-const teste = () => ({ nome: 'Jorge' });
-
-console.log(teste());
+console.log(soma(1));
+console.log(soma());
