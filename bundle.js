b15b88e52b922486c6d478cdc883efc9e9e42048
Jorge Ramos <jorginhodeveloper@gmail.com>
2019-04-25 17:32:31 -0300

    Add files to lesson 10 - rest/spread operator

--- bundle.js
+++ bundle.js
@@ -1,24 +1,57 @@
 "use strict";
 
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+// REST
 var usuario = {
   nome: 'Jorge',
   idade: 34,
-  endereco: {
-    cidade: 'Camaquã',
-    estado: 'RS'
-  }
+  empresa: 'Rocketseat'
 };
-var nome = usuario.nome,
-    idade = usuario.idade,
-    cidade = usuario.endereco.cidade;
-console.log(nome);
-console.log(idade);
-console.log(cidade);
 
-function mostraDados(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade;
-  console.log(nome, idade);
+var nome = usuario.nome,
+    resto = _objectWithoutProperties(usuario, ["nome"]);
+
+console.log(nome);
+console.log(resto);
+var arr = [1, 2, 3, 4];
+var a = arr[0],
+    b = arr[1],
+    c = arr.slice(2);
+console.log(a);
+console.log(b);
+console.log(c);
+
+function soma() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
 }
 
-mostraDados(usuario);
+console.log(soma(1, 3, 4)); // SPREAD
+
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+var arr3 = [].concat(arr1, arr2);
+console.log(arr3);
+var usuario1 = {
+  nome: 'Jorge',
+  idade: 34,
+  empresa: 'Rocketseat'
+};
+
+var usuario2 = _objectSpread({}, usuario1, {
+  nome: 'Daniel'
+});
+
+console.log(usuario2);
--- main.js
+++ main.js
@@ -1,19 +1,38 @@
+// REST
 const usuario = {
   nome: 'Jorge',
   idade: 34,
-  endereco: {
-    cidade: 'Camaquã',
-    estado: 'RS',
-  },
+  empresa: 'Rocketseat'
 };
-
-const { nome, idade, endereco: { cidade } } = usuario;
-
+const { nome, ...resto } = usuario;
 console.log(nome);
-console.log(idade);
-console.log(cidade);
+console.log(resto);
 
-function mostraDados({ nome, idade }) {
-  console.log(nome, idade);
+
+const arr = [1, 2, 3, 4];
+const [ a, b, ...c ] = arr;
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+function soma(...params) {
+  return params.reduce((total, next) => total + next);
 }
-mostraDados(usuario);
+console.log(soma(1, 3, 4));
+
+
+// SPREAD
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = [...arr1, ...arr2];
+console.log(arr3);
+
+
+const usuario1 = {
+  nome: 'Jorge',
+  idade: 34,
+  empresa: 'Rocketseat'
+};
+const usuario2 = { ...usuario1, nome: 'Daniel' };
+console.log(usuario2);
