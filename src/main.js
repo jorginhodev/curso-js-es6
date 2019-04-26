6b4d8792490043d97dcfc10d2c07235575c46fa8
Jorge Ramos <jorginhodeveloper@gmail.com>
2019-04-26 14:20:17 -0300

    Add files to lesson 16 - async/await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-alert('Teste');
+const minhaPromise = () => new Promise((resolve, reject) => {
+  setTimeout(() => { resolve('OK') }, 2000);
+});
+
+// async function executaPromise() {
+//   console.log(await minhaPromise());
+//   console.log(await minhaPromise());
+//   console.log(await minhaPromise());
+// }
+
+const executaPromise = async () => {
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+}
+
+executaPromise();
