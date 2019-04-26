148f829d5e1e3547fc341de5a6161f46615b23fb
Jorge Ramos <jorginhodeveloper@gmail.com>
2019-04-26 16:16:07 -0300

    Add files to lesson 21 - buscando da api

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+import api from './api';
+
 class App {
   constructor() {
     this.repositories = [];
 
     this.formEl = document.querySelector('#repo-form');
+    this.inputEl = document.querySelector('input[name=repository]');
     this.listEl = document.querySelector('#repo-list');
 
     this.registerHandlers();
@@ -12,15 +15,26 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
-  addRepository(event) {
+  async addRepository(event) {
     event.preventDefault();
 
+    const repoInput = this.inputEl.value;
+
+    if (repoInput.length === 0)
+      return;
+
+    const response = await api.get(`/repos/${repoInput}`);
+
+    const { name, description, html_url, owner: { avatar_url } } = response.data;
+
     this.repositories.push({
-      name: 'rocketseat.com.br',
-      description: 'Tire a sua ideia do papel e dê vida à sua startup.',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-      html_url: 'https://github.com/rocketseat/'
+      name,
+      description,
+      avatar_url,
+      html_url,
     });
+
+    this.inputEl.value = '';
 
     this.render();
   }
