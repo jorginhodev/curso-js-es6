e4fbe704b63d0f1c1cb6fdc69d061534d3f5dfd2
Jorge Ramos <jorginhodeveloper@gmail.com>
2019-04-26 13:58:27 -0300

    Add files to lesson 15 - webpack dev server

--- main.js
+++
@@ -1,6 +0,0 @@
-// import { soma, sub, mult } from './funcoes';
-import * as funcoes from './funcoes';
-
-console.log(funcoes.soma(2, 3));
-console.log(funcoes.sub(2, 1));
-console.log(funcoes.mult(2, 4));
