f188da4ec934092e62a1edd0fd2be7aa35008b8c
Jorge Ramos <jorginhodeveloper@gmail.com>
2019-04-26 15:37:37 -0300

    Add files to lesson 20 - render em tela

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ class App {
     this.repositories = [];
 
     this.formEl = document.querySelector('#repo-form');
+    this.listEl = document.querySelector('#repo-list');
 
     this.registerHandlers();
   }
@@ -21,7 +22,35 @@ class App {
       html_url: 'https://github.com/rocketseat/'
     });
 
-    console.log(this.repositories);
+    this.render();
+  }
+
+  render() {
+    this.listEl.innerHTML = '';
+
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', repo.avatar_url);
+
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement('p');
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('target', '_blank');
+      linkEl.setAttribute('href', repo.html_url);
+      linkEl.appendChild(document.createTextNode('Acessar'));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    });
   }
 }
 
